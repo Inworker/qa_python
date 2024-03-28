ad3465c82e4e928a17549f334bd50529e9b0be67
Roman Zogol <rzogol@yandex.ru>
2024-03-28 10:42:20 +0300

    First commit in develop

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
+import pytest
 from main import BooksCollector
-
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
@@ -8,11 +8,27 @@ class TestBooksCollector:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_init_all_genre(self):
+    @pytest.mark.parametrize('ganre', ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии'])
+    def test_init_get_all_genre(self, ganre): # получить список всех жанров из аттрибутов
         collector = BooksCollector()
-        assert collector.genre == ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
+        assert ganre in collector.genre
 
-    def test_add_new_book_add_two_books(self):
+    @pytest.mark.parametrize('age_ganre', ['Ужасы', 'Детективы'])
+    def test_init_get_age_rating(self,age_ganre):
+        collector = BooksCollector()
+        assert age_ganre in collector.genre_age_rating
+
+    def test_null_favotites_list(self):
+        collector = BooksCollector()
+        assert collector.favorites == []
+
+    def test_null_books_genre(self):
+        collector = BooksCollector()
+        assert collector.books_genre == {}
+
+
+
+    def test_add_new_book_add_two_books(self):# добавить 2 книги в пустой список
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
@@ -26,46 +42,43 @@ class TestBooksCollector:
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
-    def test_set_book_genre(self):
+    def test_set_book_genre_one_book(self): #добавить жанр "Ужасы" "для книги Гордость и предубеждение и зомби"
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
         assert collector.books_genre['Гордость и предубеждение и зомби'] ==  'Ужасы'
 
 
-
-
-    def test_get_book_genre(self):
+    def test_get_book_genre(self):# получить название по жанру  добавленной книги
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
-        listok = list(collector.books_genre.keys())
-        assert listok == ['Гордость и предубеждение и зомби']
+        assert collector.get_book_genre('Гордость и предубеждение и зомби') == 'Ужасы'
 
 
-    def test_get_books_with_specific_genre_two_book_detective(self):
+    def test_get_books_with_specific_genre_add_two_book_same_ganre(self):# Добавить 2 книги с жанром "Детектив"
         collector = BooksCollector()
-        collector.add_new_book('Смешарики')
-        collector.set_book_genre('Смешарики', 'Ониме')
+        books_with_specific_genre = []
+
         collector.add_new_book('Шерлок')
         collector.set_book_genre('Шерлок', 'Детективы')
         collector.add_new_book('Майор Гром')
         collector.set_book_genre('Майор Гром', 'Детективы')
-        assert collector.get_books_with_specific_genre("Детективы") == ['Шерлок', 'Майор Гром']
+        assert collector.get_books_with_specific_genre('Детективы') == ['Шерлок','Майор Гром']
 
 
-    def test_get_books_genre_33(self):
-         collector = BooksCollector()
-         collector.add_new_book('Смешарики')
-         collector.set_book_genre('Смешарики', 'Фантастика')
-         collector.add_new_book('Шерлок')
-         collector.set_book_genre('Шерлок', 'Детективы')
-         collector.add_new_book('Майор Гром')
-         collector.set_book_genre('Майор Гром', 'Детективы')
-         assert collector.get_books_genre() == {'Смешарики': 'Фантастика', 'Шерлок': 'Детективы', 'Майор Гром': 'Детективы'}
+    def test_get_books_genre_add_3_book_different_ganre(self):# Получить словарь всех добавленных книг по жанру
+        collector = BooksCollector()
+        collector.add_new_book('Смешарики')
+        collector.set_book_genre('Смешарики', 'Мультфильмы')
+        collector.add_new_book('Шерлок')
+        collector.set_book_genre('Шерлок', 'Детективы')
+        collector.add_new_book('Чужой')
+        collector.set_book_genre('Чужой', 'Ужасы')
+        assert collector.get_books_genre() == {'Смешарики': 'Мультфильмы', 'Шерлок': 'Детективы', 'Чужой': 'Ужасы'} and len(collector.get_books_genre()) == 3
 
 
-    def test_get_books_for_children_for_children_positive(self):
+    def test_get_books_for_children_add_2_children_book(self): # Получить список книг с детскими жанрами
         collector = BooksCollector()
         collector.add_new_book('Смешарики')
         collector.set_book_genre('Смешарики', 'Мультфильмы')
@@ -74,30 +87,23 @@ class TestBooksCollector:
         assert collector.get_books_for_children() == ['Смешарики', 'Машенька']
 
 
-    def test_add_book_in_favorites(self):#Добавил в избранные 1 книгу - 1 книга в списке
+    def test_add_book_in_favorites_add_one_favorites_book(self):#Добавил в избранные 1 книгу - 1 книга в списке
         collector = BooksCollector()
         collector.add_new_book('Смешарики')
         collector.set_book_genre('Смешарики', 'Фантастика')
         collector.add_book_in_favorites('Смешарики')
         assert collector.get_list_of_favorites_books() == ['Смешарики']
-        # print(collector.get_list_of_favorites_books())
 
-
-    def test_delete_book_from_favorites(self):# добавил и удалил 1 книгу из избранного = список избранных пустой
+    def test_delete_book_from_favorites_add_and_remove_same_favorite_book(self):#adding and removing 1 book from favorites  добавил и удалил 1 книгу из избранного = список избранных пустой
         collector = BooksCollector()
         collector.add_new_book('Смешарики')
-        collector.set_book_genre('Смешарики', 'Фантастика')
         collector.add_book_in_favorites('Смешарики')
         collector.delete_book_from_favorites('Смешарики')
-
         assert collector.get_list_of_favorites_books() == []
-        # print(collector.get_list_of_favorites_books())
 
-    def test_get_list_of_favorites_books(self): #добавить одинаковые избранные книги
+    def test_get_list_of_favorites_books_add_2_same_favorite_book_(self): #добавить одинаковые избранные книги
         collector = BooksCollector()
         collector.add_new_book('Смешарики')
         collector.add_new_book('Смешарики')
-        collector.add_new_book('Смешарики')
-        collector.add_book_in_favorites('Смешарики')
         collector.add_book_in_favorites('Смешарики')
         assert collector.get_list_of_favorites_books() == ['Смешарики']
